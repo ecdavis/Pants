0e8c01f5051d0c426e33dac1ae225872f27ec8d8
Edwin O Marshall <emarshall85@gmail.com>
2013-11-06 08:35:35 -0500

    - removed py3-views based functions

--- pants/compat.py
+++ pants/compat.py
@@ -2,18 +2,6 @@
     python 2 and 3
 '''
 
-def items(mapping):
-    ''' returns the mapping's item view '''
-    return getattr(mapping, 'viewitems', mapping.items)()
-
-def keys(mapping):
-    ''' returns the mapping's key view '''
-    return getattr(mapping, 'viewkeys', mapping.keys)()
-
-def values(mapping):
-    ''' returns the mapping's value view '''
-    return getattr(mapping, 'viewvalues', mapping.values)()
-
 try:
     basestring = basestring
 except NameError:
